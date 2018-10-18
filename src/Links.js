e7e224e81dff276b88f615aef6f651a51c9ddd70
jmangz <echou1@binghamton.edu>
2018-10-18 13:20:27 -0400

    added proptypes for props

--- src/Links.js
+++ src/Links.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Links.css';
 
 const Links = ({
   githubHoverState,
   linkedinHoverState,
   hoverGithub,
-  hoverChange,
+  hoverLinkedin,
   toDefault,
   rightPageState,
 }) => (
@@ -29,10 +30,19 @@ const Links = ({
       <p className="name-character">r</p>
     </div>
     <div className="links">
-      <a className={linkedinHoverState} onMouseEnter={hoverChange} onMouseLeave={toDefault} href="https://linkedin.com/in/echou1/">Linkedin</a>
+      <a className={linkedinHoverState} onMouseEnter={hoverLinkedin} onMouseLeave={toDefault} href="https://linkedin.com/in/echou1/">Linkedin</a>
       <a className={githubHoverState} onMouseEnter={hoverGithub} onMouseLeave={toDefault} href="https://github.com/jmangz">Github</a>
     </div>
   </div>
 );
+
+Links.propTypes = {
+  githubHoverState: PropTypes.string.isRequired,
+  linkedinHoverState: PropTypes.string.isRequired,
+  hoverGithub: PropTypes.func.isRequired,
+  hoverLinkedin: PropTypes.func.isRequired,
+  toDefault: PropTypes.func.isRequired,
+  rightPageState: PropTypes.string.isRequired,
+};
 
 export default Links;
